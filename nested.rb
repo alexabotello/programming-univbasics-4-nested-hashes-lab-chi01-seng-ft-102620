445b5ec484065dac1ee57a55e45bb7bf6692c9ee
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:07:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,15 +81,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] = 
-  programmer_hash = 
-  {
-  :yukihiro_matsumot => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
-}
-programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto][0] = {known_for: "Ruby", languages: ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
