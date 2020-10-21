a2e3738aae336395fb8c2f63592a1704d48a312a
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:18:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto][0] = { known_for: "Ruby", languages: ["LISP", "C"] }
+  programmer_hash[:yukihiro_matsumoto][0] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
   programmer_hash
 end
 
@@ -126,5 +126,5 @@ def adding_to_dennis
         :languages => ["C", "Assembly"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages] 
 end
