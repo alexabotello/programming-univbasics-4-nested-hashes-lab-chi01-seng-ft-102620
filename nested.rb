cb90d1cfe65ce9fa543bbf74dd1222353eb5c689
Alexa Botello <abotello20@yahoo.com>
2020-10-21 03:22:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,6 @@ def adding_to_dennis
         :languages => ["C", "Assembly"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages] 
+    programmer_hash[:dennis_ritchie][:languages] =+ "Assembly"
+    programmer_hash
 end
